b5bcaedc6b4774b1a2b36a766a0fcd403c8181a4
Carlos Teixeira <carlosmtxcmcmtx@gmail.com>
2014-03-05 14:58:11 +0000

    Uipi Ai E

--- Projeto/src/Peer.java
+++ Projeto/src/Peer.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
-import java.util.Random;
+import java.util.Stack;
 import java.util.Vector;
 
 /**
@@ -19,34 +21,49 @@ public class Peer {
     Vector<Thread> BackupThreads;
     Vector<Thread> RecoveryThreads;
 
+    Stack<String> sentStack;
+
     Peer(String MCControlIP,String MCRecoveryIP,String MCBackupIP)throws UnknownHostException,IOException{
+        int port = 2000;
         this.MCBackupVIP   = InetAddress.getByName(MCBackupIP);
         this.MCRecoveryVIP = InetAddress.getByName(MCRecoveryIP);
         this.MCControlVIP  = InetAddress.getByName(MCControlIP);
 
-        MCControlSock = new MulticastSocket(5000);
+        MCControlSock = new MulticastSocket(port);
         MCControlSock.joinGroup(MCControlVIP);
+
+        sentStack = new Stack<String>();
 
     }
     void activate()throws SocketException{
         byte[] buff = new String("Hello Everybody").getBytes();
         byte[] buff2= new byte[1024];
 
-        DatagramPacket packet1 = new DatagramPacket(buff ,buff.length,MCControlVIP,4446);
+        DatagramPacket packet1 = null;
         DatagramPacket packet2 = new DatagramPacket(buff2,buff2.length);
 
         System.out.println("Local Port:"+MCControlSock.getLocalPort());
         System.out.println("Loopback M:"+MCControlSock.getLoopbackMode());
         System.out.println("BroadCast :"+MCControlSock.getBroadcast());
         System.out.println(new String(buff));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        String data = null;
         try{
-
+            data=read.readLine();
+            data+='\n';
+            sentStack.push(data);
+            packet1 = new DatagramPacket(data.getBytes(),data.length(),MCControlVIP,2000);
             MCControlSock.send(packet1);
-            MCControlSock.receive(packet2);
-            System.out.println(packet2.getData());
+            do{
+                MCControlSock.receive(packet2);
+                data = new String(packet2.getData());
+               if (data.substring(0,data.lastIndexOf('\n')+1).equals(sentStack.peek())){}
+               else{}
+            }while(true);
         }
-        catch(Exception e){
+        catch(IOException e){
         }
+        System.out.println(data);
     }
     void backupThreadLaunch(String fileID){
 
@@ -54,7 +71,9 @@ public class Peer {
     void recoveryThreadLaunch(String fileID){
 
     }
-
+    void controlThreadHandler(String message){
+     String[] controls = message.split("\\s");
+    }
 
 
 }
